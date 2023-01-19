d02b575ea137d6ae9c8ad6d5717d53f287159c8b
Mark Sorial <msorial44@gmail.com>
2023-01-19 12:00:38 +0400

    added about page

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import glasses from '../../public/imgs/glasses.svg';
+import profile from '../../public/imgs/profilePic.png'
 import Sidebar from '../sidebar/Sidebar';
 import Menu from '../menu/Menu';
+
 import './About.scss';
 
 function About() {
@@ -13,7 +15,50 @@ function About() {
             <Menu />
           </div>
           <div className='main-content'>
-            
+            <div className='about-page'>
+              <div className="photo-content">
+                <img src={profile} alt="profile" />
+              </div>
+              <div className="text-content">
+                <div className='top-paragraph' style={{ marginBottom: '3rem'}}>
+                  <p>
+                    Hello, my name is Mark Sorial and I like to make things. 
+                    I started creating Minecraft Mods in 5th grade and ever 
+                    since then I haven’t stopped making. 
+                  </p>
+                </div>
+                <div className="paragraph2">
+                  <p>
+                    Today, I have created numerous projects in both the
+                    hardware and software spaces. Through my career I have
+                    gained a proficiency in <p style={{ color: '#88C0D0'}}>HTML</p>, <p style={{ color: '#88C0D0'}}>CSS</p>, <p style={{ color: '#88C0D0'}}>JavaScript</p>, <p style={{ color: '#88C0D0'}}>TypeScript</p>,
+                    <p style={{ color: '#88C0D0'}}> Python</p> and <p style={{ color: '#88C0D0'}}>Rust</p>.
+                  </p>
+                </div>
+                <div className="paragraph3">
+                  <div style={{fontSize: '1.5rem'}}>
+                    <p>Experience</p>
+                  </div>
+                  <div style={{ marginBottom: '1rem', marginTop: "2rem"}}>
+                    <p>-Anghami, Web Development Intern</p>
+                  </div>
+                  <div style={{ marginBottom: '1rem'}}>
+                    <p>-Dana Farber, Bioinformatics Intern</p>
+                  </div>
+                </div>
+                <div className="paragraph4">
+                <div style={{fontSize: '1.5rem'}}>
+                    <p>Education</p>
+                  </div>
+                  <div style={{ marginBottom: '1rem', marginTop: "2rem"}}>
+                    <p>-American School of Dubai, 2023</p>
+                  </div>
+                  <div style={{ marginBottom: '1rem'}}>
+                    <p>-University of Something, 2027</p>
+                  </div>
+                </div>             
+              </div>
+            </div>
           </div>
         </div>
     </div>
